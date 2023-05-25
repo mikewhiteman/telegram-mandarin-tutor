06032318c9408fb11c5c60e2a162f414bed84e06
Michael Whiteman <michaelswhiteman@gmail.com>
2023-05-24 22:59:29 -0400

    fetching audio file from telegram

--- app.py
+++ app.py
@@ -19,6 +19,26 @@ telegram_bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 openai_agent = ChatgptAgent()
 
 
+# Telegram speech recognition logic
+@telegram_bot.message_handler(content_types=["voice"])
+def voice_to_text(message):
+    user_id = message.chat.id
+    audio_file_info = telegram_bot.get_file(message.voice.file_id)
+    audio_file = requests.get(f"https://api.telegram.org/file/bot{TELEGRAM_BOT_TOKEN}/{audio_file_info.file_path}")
+
+    #Save file locally
+    with open("voice_message.ogg", "wb") as f:
+        f.write(audio_file.content)
+
+
+
+
+
+
+
+
+
+
 # This will store previous messages - hacky but works for 1:1 convos w/o long-term memory
 global previous_messages
 previous_messages = []
