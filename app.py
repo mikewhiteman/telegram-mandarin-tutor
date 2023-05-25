46258dfe013f84f400306f2e1862a840bf5216b0
Michael Whiteman <michaelswhiteman@gmail.com>
2023-05-24 22:32:51 -0400

    created init for chatgpt class

--- app.py
+++ app.py
@@ -3,6 +3,29 @@ import openai
 from dotenv import load_dotenv
 import telebot
 import requests
+import speech_recognition as sr
 from gtts import gTTS
 from pydub import AudioSegment
-import speech_recognition as sr
+from chatgpt_settings import ChatgptAgent
+
+load_dotenv()
+
+# Static keys defined in .env file
+OPENAPI_KEY = os.getenv('OPENAPI_KEY')
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+#Initialize Telegram bot
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+
+#Initial chat request
+
+previous_messages = []
+
+def chat_request(previous_messages: list) -> str:
+
+
+
+
+
+
+    return 'test'
---
+++ chatgpt_settings.py
@@ -0,0 +1,11 @@
+class ChatgptAgent:
+    def __init__(self, model='gpt-3.5-turbo', temperature=.7, max_tokens=256, top_p=1, frequency_penalty=0, presence_penalty=0):
+        self.model = model
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.top_p = top_p
+        self.frequency_penalty = frequency_penalty
+        self.presence_penality = presence_penalty
+        self.base_persona = 'Hi ChatGPT - test123'
+        
+
