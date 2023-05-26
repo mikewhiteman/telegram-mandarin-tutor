d00c96009700761ab7fce7d9d8270e3ba25ec153
Michael Whiteman <michaelswhiteman@gmail.com>
2023-05-25 21:18:26 -0400

    updated chatgpt default settings

--- chatgpt_settings.py
+++ chatgpt_settings.py
@@ -1,9 +1,8 @@
 class ChatgptAgent:
-    def __init__(self, model='gpt-3.5-turbo', temperature=.7, max_tokens=256, top_p=1, frequency_penalty=0, presence_penalty=0):
+    def __init__(self, model='gpt-3.5-turbo', temperature=.7, max_tokens=1028, frequency_penalty=0, presence_penalty=0):
         self.model = model
         self.temperature = temperature
         self.max_tokens = max_tokens
-        self.top_p = top_p
         self.frequency_penalty = frequency_penalty
         self.presence_penality = presence_penalty
         self.base_prompt = [{"role": "system", "content": "You are a Chinese tutor who is helping beginner students learn Chinese. \
